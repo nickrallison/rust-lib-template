d9c106daf8b46fcb41ee84bd26f882daa787277d
Nick Allison <99363282+nickrallison@users.noreply.github.com>
2024-07-28 21:50:05 +0000

    Create shell.nix

---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    rustc
+    cargo
+    rustfmt
+    rust-analyzer
+    clippy
+    gcc_multi
+  ];
+}
